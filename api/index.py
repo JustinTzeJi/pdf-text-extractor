d12b23f28ee0698afa499b85b9810de020e2f9a1
Englios <englios656@gmail.com>
2024-10-15 15:36:23 +0800

    Updated api to current one in main repo

--- api/index.py
+++ api/index.py
@@ -2,18 +2,19 @@
 #================================================================================================
 #================================================================================================
 import urllib
-from api.dbutils.data_validation import contentModel, attachementModel
+from dbutils.data_validation import contentModel, attachementModel
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel,HttpUrl
-from api.pdf_parser import PDFExtractor
+from pdf_parser import PDFExtractor
 from typing import List,Optional
 
 
 # Initialize the FastAPI app and models
 #================================================================================================
 #================================================================================================
-app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
+app = FastAPI()
+
 class PDFRequest(BaseModel):
     url: HttpUrl
     get_images: bool = False
@@ -26,14 +27,14 @@ class ContentSummary(BaseModel):
 
 class Metadata(BaseModel):
     url: str
-    file_name: str
-    file_type: str
-    file_size: int
-    num_pages: int
-    creation_date: Optional[str]
-    modified_date: Optional[str]
-    processing_time: float
-    content_summary: ContentSummary
+    file_name:str
+    file_type:str
+    file_size:int
+    num_pages:int
+    creation_date:Optional[str]
+    modified_date:Optional[str]
+    processing_time:float
+    content_summary:ContentSummary
     
 class PDFResponse(BaseModel):
     metadata: Metadata
@@ -44,6 +45,15 @@ class Response(BaseModel):
     success: bool
     data : PDFResponse
 
+class Metadata_clone(BaseModel):
+    url: str
+    file_name:str
+    file_type:str
+    file_size:int
+    num_pages:int
+    creation_date:str
+    modified_date:str
+
 
 # API Endpoints
 #================================================================================================
@@ -51,6 +61,67 @@ class Response(BaseModel):
 @app.get("/api/py/helloFastApi")
 def hello_fast_api():
     return {"message": "Hello from FastAPI"}
+
+@app.post("/api/py/pdf_metadata")
+async def pdf_metadata(request:PDFRequest)->dict:
+    try:
+        # Extract content from the PDF
+        extractor = PDFExtractor()
+        extractor.process_pdf(
+            link = str(request.url), # change url from pydantic object to str since it expects a string
+            get_images=request.get_images,
+        )
+        response = Metadata_clone(
+                    url=str(request.url),
+                    creation_date=extractor.createDate,
+                    modified_date=extractor.modDate,
+                    file_name=extractor.filename,
+                    file_type=extractor.filetype,
+                    file_size=extractor.file_size,
+                    num_pages=extractor.num_pages,
+                )
+        return JSONResponse(
+            status_code=200,
+            content=response.model_dump()
+        )
+
+    except urllib.error.URLError as e:
+        return JSONResponse(
+            status_code=400,
+            content={
+                "success": False,
+                "error": {
+                    "code": 400,
+                    "message": f"URL Error: {str(e)}"
+                }
+            }
+        )
+        
+    except HTTPException as e:
+        return JSONResponse(
+            status_code=400,
+            content={
+                "success": False,
+                "error": {
+                    "code": 400,
+                    "message": f"Error processing PDF: {str(e.detail)}"
+                }
+            }
+        )
+        
+
+    except Exception as e:
+        return JSONResponse(
+            status_code=500,
+            content={
+                "success": False,
+                "error": {
+                    "code": 500,
+                    "message": f"Error processing PDF: {str(e)}"
+                }
+            }
+        )
+    
 
 @app.post("/api/py/pdf_extract")
 async def extract_pdf(request:PDFRequest)->dict:
@@ -91,7 +162,7 @@ async def extract_pdf(request:PDFRequest)->dict:
                     file_type=extractor.filetype,
                     file_size=extractor.file_size,
                     num_pages=extractor.num_pages,
-                    processing_time=round(extractor.time_taken,3),\
+                    processing_time=round(extractor.time_taken,3),
                     content_summary=ContentSummary(
                         plain_text_length=len(content_model.plain),
                         html_length=len(content_model.html),
@@ -146,3 +217,7 @@ async def extract_pdf(request:PDFRequest)->dict:
                 }
             }
         )
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True)
